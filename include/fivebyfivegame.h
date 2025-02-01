5af93e5d73e14c8ce91bf0c01a1c48f87cdc8558
Nagham Wael <161651186+NaghamProgrammer@users.noreply.github.com>
2025-02-01 20:30:15 +0200

    Update fivebyfivegame.h

--- include/fivebyfivegame.h
+++ include/fivebyfivegame.h
@@ -18,8 +18,8 @@ public:
     explicit fivebyfiveGame(QWidget *parent = nullptr, const QString &firstPlayer = "", const QString &secondPlayer = "");
     ~fivebyfiveGame();
     void updateButton(QPushButton* button, int &turnCounter);
-    int count_three();
-    bool isPattern(const QString& a, const QString& b, const QString& c);
+    int countPatterns(const QString &symbol);
+    bool isPattern(const QString& a, const QString& b, const QString& c, const QString &symbol);
     bool isWin();
     bool isDraw();
     bool gameIsOver();
@@ -27,39 +27,39 @@ public:
     void random();
 
 private slots:
-    void on_helpButton_clicked();
     void on_exitButton_clicked();
 
-    //row1
+    // Row 1
     void on_x11_clicked();
     void on_x12_clicked();
     void on_x13_clicked();
     void on_x14_clicked();
     void on_x15_clicked();
-    //row2
+    // Row 2
     void on_x21_clicked();
     void on_x22_clicked();
     void on_x23_clicked();
     void on_x24_clicked();
     void on_x25_clicked();
-    //row3
+    // Row 3
     void on_x31_clicked();
     void on_x32_clicked();
     void on_x33_clicked();
     void on_x34_clicked();
     void on_x35_clicked();
-    //row4
+    // Row 4
     void on_x41_clicked();
     void on_x42_clicked();
     void on_x43_clicked();
     void on_x44_clicked();
     void on_x45_clicked();
-    //row5
+    // Row 5
     void on_x51_clicked();
     void on_x52_clicked();
     void on_x53_clicked();
     void on_x54_clicked();
     void on_x55_clicked();
+
 signals:
     void backToManuWindow(); 
 
@@ -69,6 +69,10 @@ private:
     QPushButton *lastClickedButton;
     Win *win;
     Draw *draw;
+    int scoreX = 0; // Player X score
+    int scoreO = 0; // Player O score
+    int turnCounter = 0; // Turn counter
+    bool flag = false; // Flag for random player
 };
 
-#endif 
+#endif
